616e343f763ada1a5f0e8296bb4aefc56b07baa8
Dharmendra Prajapati <dharmendra_prajapati@epam.com>
2018-09-06 11:13:50 +0530

    Add code for ngFor, ngIf,ngSwitch and expression null handling.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,19 +2,12 @@ import { Component, Input, Output, EventEmitter} from '@angular/core'
 
 @Component({
     selector : 'event-thumbnail',
-    template : `<div class="well howerwell thumbnail">
-                    <h2> {{firstEvent.name}} </h2>
-                    <div> Date : {{firstEvent.date}} </div>
-                    <div> Time : {{firstEvent.time}} </div>
-                    <div> Price : \${{firstEvent.price}} </div>
-                    <div>  </div>
-                    <div> Location :
-                        <span> {{firstEvent.location.address}} </span>                        
-                        <span class="pad-left"> {{firstEvent.location.city}} , {{firstEvent.location.country}} </span>
-                    </div>
-                    <button class="btn btn-primary" (click)="ClickMe(firstEvent.id)">Join</button>
-                </div>`,
-    styles : [' .pad-left {margin-left:10px;} .well div {color: #bbb;}']
+    templateUrl : './event-thumbnail.component.html',
+    styles : [` 
+    .pad-left {margin-left:10px;} 
+    .well div {color: #bbb;}
+    .thumbnail {min-height:210px;}
+    `]
 })
 export class EventThumbnailComponent{
     @Input() firstEvent:any;
